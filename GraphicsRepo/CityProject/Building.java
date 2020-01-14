39b2caf0cf857cc84af294887818b8ea4b91dfd1
ssrenayang <54153238+ssrenayang@users.noreply.github.com>
2020-01-14 12:22:31 -0600

    ugh got some buildings...1/14

--- GraphicsRepo/CityProject/Building.java
+++ GraphicsRepo/CityProject/Building.java
@@ -6,32 +6,28 @@
  * @version (a version number or a date)
  */
 import java.awt.*;
+import java.util.Random;
 public class Building
 {
 
-	private int base, height;
+	private int base, height, xpoint, ypoint;
 	private Color color;
-    public Building(int width, int length, Color shade){
+	private Random generator = new Random();
+
+    public Building(int x, int y, int width, int length, Color shade){
 		 base = width;
 		 height = length;
 		 color = shade;
+		 xpoint = x;
+		 ypoint = y;
 	}
 
 	public void draw(Graphics page){
-		int square = 20;
 		page.setColor(color);
-		//	page.fillRect(20, 20, base, height);
-		int move = 0;
-		for(int i = 0, j = 0; i < base; i+=20, j+=1, move += 20){
-			page.fillRect(i , 0, 10, 10);
-			if (j % 2 != 0)
-				page.setColor(Color.white);
-			else
-				page.setColor(color);
-
+		page.fillRect(xpoint, ypoint, base, height);
 		}
 
 	}
-	}
+
 
 
--- GraphicsRepo/CityProject/Final.java
+++ GraphicsRepo/CityProject/Final.java
@@ -6,20 +6,28 @@ import javax.swing.*;
 
 
 public class Final extends Applet{
-
-	private Building build1;
-
+	private Random gen = new Random();
+	private Building build1, build2, build3, build4;
+	private int h1, h2, h3, h4, h5, base;
 	private final int APPLET_WIDTH = 1000;
-	private final int APPLET_HEIGHT = 1000;
-	private final int HEIGHT_MIN = 1000;
+	private final int APPLET_HEIGHT = 500;
+	private final int HEIGHT_MIN = 100;
 	private final int VARIANCE = 40;
 
 	public void init(){
-		build1 = new Building(60, 60, Color.red);
+		base = 150;
+		h1 = gen.nextInt(100) + 150;
+		h2 = gen.nextInt(100) + 150;
+		h3 = gen.nextInt(100) + 150;
+		h4 = gen.nextInt(100) + 150;
+		build1 = new Building(50, 150, base, 100, Color.red);
+		build2 = new Building(base + 100, 50, base, 200, Color.green);
+		setSize (APPLET_WIDTH, APPLET_HEIGHT);
 
 	}
 	public void paint(Graphics page){
 		build1.draw(page);
+		build2.draw(page);
 
 	}
 
