8a9f0a4de9492f9782192e27c9e6e2f168b7c227
ssrenayang <54153238+ssrenayang@users.noreply.github.com>
2020-01-16 07:42:43 -0600

    have a road 1/16/20

--- GraphicsRepo/CityProject/Final.java
+++ GraphicsRepo/CityProject/Final.java
@@ -8,54 +8,45 @@ import javax.swing.*;
 public class Final extends Applet{
 	private Random gen = new Random();
 
-	private Building build1, build2, build3, build4;
-	private int h1, h2, h3, h4, h5, base;
+	private Building build1, build2, build3, build4, build5, build6;
 
 	private final int APPLET_WIDTH = 1000;
 	private final int APPLET_HEIGHT = 500;
 
 	public void init(){
-		base = 150;
-
-		h1 = gen.nextInt(100) + 150;
-		h2 = gen.nextInt(100) + 150;
-		h3 = gen.nextInt(100) + 150;
-		h4 = gen.nextInt(100) + 150;
 
 		//when moving buildings over, do #of windows of previous building * 20 + 10 to be next to each other
 		//(start x, start y, num of windows across, number of windows down, color)
 
-		build1 = new Building(50, 300, 4, 4, Color.red);
+		build1 = new Building(50, 300, 4, 9, Color.red);
 		build2 = new Building(150, 300, 4, 6, Color.green);
-		build3 = new Building(250, 300, 5, 9, Color.blue);
-		build4 = new Building(370, 300, 6, 3, Color.yellow);
+		build3 = new Building(250, 300, 9, 10, Color.blue);
+		build4 = new Building(450, 300, 2, 6, Color.yellow);
+		build5 = new Building(510, 300, 11, 13, Color.pink);
+		build6 = new Building(750, 300, 9, 11, Color.gray);
+
 		setSize (APPLET_WIDTH, APPLET_HEIGHT);
+		setBackground(Color.cyan);
 
 	}
 	public void paint(Graphics page){
 
-		for(int i = 0; i < 370; i+=10){
 			build1.draw(page);
 			build2.draw(page);
 			build3.draw(page);
 			build4.draw(page);
+			build5.draw(page);
+			build6.draw(page);
 
-			build1.setX(50-i);
-			build2.setX(150-i);
-			build3.setX(250-i);
-			build4.setX(370-i);
+			page.fillRect(0, 300, 1000, 50);
 
-			build1.setBase(50-i);
-			build2.setBase(150-i);
-			build3.setBase(250-i);
-			build4.setBase(370-i);
 			try{
 				//pause the program for a quarter second (millisecond)
 				Thread.sleep(100);
 			}
 			catch(InterruptedException e) {}
 
-		}
+
 
 		repaint();
 
